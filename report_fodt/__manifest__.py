724e47b26b89446c1cc9f4079bfe92557e3ff0a6
Holger Brunn <hbrunn@therp.nl>
2017-12-17 09:53:16 +0100

    [ADD] impossible PoC because qweb wil understand namespaces only in master (12.0)

---
+++ report_fodt/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "FODT reports",
+    "version": "10.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Reporting",
+    "summary": "Create reports from FODT templates",
+    "depends": [
+        'report',
+    ],
+    "demo": [
+        "demo/ir_ui_view.xml",
+    ],
+    "data": [
+    ],
+}
---
+++ report_fodt/tests/test_report_fodt.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp.tests.common import TransactionCase
+
+
+class TestReportFodt(TransactionCase):
+    def test_report_fodt(self):
+        pass
